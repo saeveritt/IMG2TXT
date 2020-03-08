b52a92217e014cc9c4ac383bd1d21a39b7ebb34e
saeveritt <saeveritt@gmail.com>
2020-03-08 18:29:35 -0500

    -Update the style

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -27,7 +29,7 @@ def upload_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             txt = read_image(filename)
-            return txt
+            return render_template('index.html',text_detected = txt)
     return render_template('index.html')
 
 def allowed_file(filename):

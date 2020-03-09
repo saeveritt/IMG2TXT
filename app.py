8c209931e2eeecf9e608d324a924167c1412067b
saeveritt <saeveritt@gmail.com>
2020-03-09 11:59:49 -0500

    update

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-from utils import read_image
+from utils import Utils
 
 UPLOAD_FOLDER = os.getcwd() + '/data/'
-ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,27 +15,24 @@ app.config['SESSION_TYPE'] = 'filesystem'
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
-        if 'file' not in request.files:
+        if 'ufile' not in request.files:
             flash('No file part')
             return redirect(request.url)
-        file = request.files['file']
+        file = request.files['ufile']
+        print(file and Utils(file.filename).allowed_file())
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
+            return render_template('index.html', text_detected = "",ufile="Upload File")
+        if file and Utils(file.filename).allowed_file():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            txt = read_image(filename)
-            return render_template('index.html',text_detected = txt)
-    return render_template('index.html')
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
+            txt = Utils(filename).read_image()
+            request.args.get("ufile", "")
+            return render_template('index.html',text_detected = txt, ufile="Choose Another File")
+    return render_template('index.html', text_detected = "", ufile="Upload File")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0",debug=True)
 
--- utils.py
+++ utils.py
@@ -2,12 +2,51 @@ import os
 import cv2
 import pytesseract
 from PIL import Image
+from pdf2image import convert_from_path
 
-data_dir = os.getcwd() + "/data/"
 
-def read_image(filename):
-    img = cv2.imread(data_dir + filename,0)
-    ret, thresh = cv2.threshold(img, 10, 255, cv2.THRESH_OTSU)
-    cv2.imwrite(data_dir + "thresh_" + filename, thresh)
-    text = pytesseract.image_to_string(Image.open(data_dir + "thresh_" + filename))
-    return text
+
+
+class Utils():
+    def __init__(self, filename):
+        self.data_dir = os.getcwd() + "/data/"
+        self.ALLOWED_EXTENSIONS = ['pdf', 'png', 'jpg', 'jpeg', 'gif']
+        self.filename = filename.strip()
+        self.filetype = self.file_type()
+        self.isPDF = self.filetype == "pdf"
+        self.newfile = ''
+    def read_image(self):
+        self.filetype = self.file_type()
+        if self.filetype == "pdf":
+            return self.convertPDF()
+        else:
+            return self.find_text()
+
+    def find_text(self,new=False):
+        img = cv2.imread(self.data_dir + self.filename,0)
+        ret, thresh = cv2.threshold(img, 120, 255, cv2.THRESH_TOZERO)
+        filename = self.filename
+        if new:
+            filename = self.newfile
+        print(self.data_dir + "thresh_" + filename)
+        cv2.imwrite(self.data_dir + "thresh_" + filename, thresh)
+        text = pytesseract.image_to_string(Image.open(self.data_dir + "thresh_" + filename))
+        return text
+
+    def allowed_file(self):
+        check = self.filetype in self.ALLOWED_EXTENSIONS
+        return check
+
+    def file_type(self):
+        return self.filename.rsplit('.', 1)[1].lower()
+
+    def convertPDF(self):
+        text = ""
+        pages = convert_from_path(self.data_dir + self.filename,300)
+        newfile = self.filename[:-4] + '.jpg'
+        for i,page in enumerate(pages):
+            self.newfile = str(i) +"_"+ newfile
+            print(page)
+            page.save(self.newfile, 'JPEG')
+            text += self.find_text(new=True) + "\n"
+        return text
